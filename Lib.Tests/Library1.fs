aeb409db9547f5b352f2acab5ec60330fdda84f9
Partho P. Das <partho.p.das@readify.net>
2016-04-04 14:51:00 +1000

    Initial commit

---
+++ Lib.Tests/Library1.fs
@@ -0,0 +1,9 @@
+﻿module Lib.Tests
+
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let xx () =
+    let x = new Lib.Class1()
+    x.X |> should equal "F#"
---
+++ Lib/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace Lib
+
+type Class1() = 
+    member this.X = "F#"
