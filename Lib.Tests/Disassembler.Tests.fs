b39bea5d115064532bbd396fcc5a2562274db57c
Partho P. Das <this@parthopdas.com>
2016-04-19 14:39:56 +1000

    parguments done

--- Lib.Tests/Disassembler.Tests.fs
+++ Lib.Tests/Disassembler.Tests.fs
@@ -86,3 +86,90 @@ let ``pmodRegRm tests`` (bs, reg, rm) : unit =
                               ModRM = rm }
         is.Position.Offset |> should equal bs.Length
     | _ -> failwithf "Test failed: %A %A %A" bs reg rm
+
+let ``pargument tests data`` : obj array seq = 
+    seq { 
+        yield ("eAX", [||], ArgRegister AX)
+        yield ("eCX", [||], ArgRegister CX)
+        yield ("eBX", [||], ArgRegister BX)
+        yield ("eDX", [||], ArgRegister DX)
+        yield ("eSP", [||], ArgRegister SP)
+        yield ("eBP", [||], ArgRegister BP)
+        yield ("eSI", [||], ArgRegister SI)
+        yield ("eDI", [||], ArgRegister DI)
+        yield ("AX", [||], ArgRegister AX)
+        yield ("BX", [||], ArgRegister BX)
+        yield ("CX", [||], ArgRegister CX)
+        yield ("DX", [||], ArgRegister DX)
+        yield ("SP", [||], ArgRegister SP)
+        yield ("BP", [||], ArgRegister BP)
+        yield ("SI", [||], ArgRegister SI)
+        yield ("DI", [||], ArgRegister DI)
+        yield ("CS", [||], ArgRegister CS)
+        yield ("DS", [||], ArgRegister DS)
+        yield ("ES", [||], ArgRegister ES)
+        yield ("SS", [||], ArgRegister SS)
+        yield ("AL", [||], ArgRegister AL)
+        yield ("BL", [||], ArgRegister BL)
+        yield ("CL", [||], ArgRegister CL)
+        yield ("DL", [||], ArgRegister DL)
+        yield ("AH", [||], ArgRegister AH)
+        yield ("BH", [||], ArgRegister BH)
+        yield ("CH", [||], ArgRegister CH)
+        yield ("DH", [||], ArgRegister DH)
+        yield ("Eb", [| 0x00uy |], 
+               ArgDereference { DrefType = MrmTBXSI
+                                DrefDisp = None })
+        yield ("Eb", [| 0x82uy; 0xDDuy; 0x7Euy |], 
+               ArgDereference { DrefType = MrmTBPSI
+                                DrefDisp = Some(W16 0x7eddus) })
+        yield ("Eb", [| 0xC6uy |], ArgRegister DH)
+        yield ("Ev", [| 0x42uy; 0xDDuy |], 
+               ArgDereference { DrefType = MrmTBPSI
+                                DrefDisp = Some(W8 0xdduy) })
+        yield ("Ev", [| 0x06uy; 0xADuy; 0xBAuy |], 
+               ArgDereference { DrefType = MrmTDisp
+                                DrefDisp = Some(W16 0xbaadus) })
+        yield ("Ev", [| 0xC4uy |], ArgRegister SP)
+        yield ("Ew", [| 0xC7uy |], ArgRegister DI)
+        yield ("Gb", [| 0x00uy |], ArgRegister AL)
+        yield ("Gv", [| 0xD8uy |], ArgRegister BX)
+        yield ("Jb", [| 0xEEuy |], ArgOffset(W8 0xeeuy))
+        yield ("Jv", [| 0x0Duy; 0xF0uy |], ArgOffset(W16 0xf00dus))
+        yield ("Ib", [| 0xF0uy |], ArgImmediate(W8 0xf0uy))
+        yield ("Iv", [| 0xEFuy; 0xBEuy |], ArgImmediate(W16 0xbeefus))
+        yield ("Iw", [| 0xADuy; 0xDEuy |], ArgImmediate(W16 0xdeadus))
+        yield ("I0", [| 0x42uy |], ArgImmediate(W8 0x42uy))
+        yield ("Ob", [| 0x0Duy; 0xF0uy |], 
+               ArgDereference { DrefType = MrmTDisp
+                                DrefDisp = Some(W16 0xf00dus) })
+        yield ("Ob", [| 0xADuy; 0xDEuy |], 
+               ArgDereference { DrefType = MrmTDisp
+                                DrefDisp = Some(W16 0xdeadus) })
+        yield ("Sw", [| 0x00uy |], ArgRegister ES)
+        yield ("Sw", [| 0x08uy |], ArgRegister CS)
+        yield ("Sw", [| 0x10uy |], ArgRegister SS)
+        yield ("Sw", [| 0x18uy |], ArgRegister DS)
+        yield ("Ap", [| 0x0Duy; 0xF0uy; 0xADuy; 0xDEuy |], 
+               ArgAddress({ Segment = 0xdeadus
+                            Offset = 0xf00dus }))
+        yield ("Mp", [| 0x2Euy; 0xF0uy; 0xDDuy |], 
+               ArgDereference { DrefType = MrmTDisp
+                                DrefDisp = Some(W16 0xDDF0us) })
+        yield ("Mp", [| 0xD8uy |], ArgRegister AX)
+        yield ("1", [||], ArgConstant 1uy)
+        yield ("3", [||], ArgConstant 3uy)
+    }
+    |> Seq.map (fun (a, b, c) -> 
+           [| box a
+              box b
+              box c |])
+
+[<Theory>]
+[<MemberData("pargument tests data")>]
+let ``pargument tests`` (desc, bs, res) : unit = 
+    match runOnInput (pargument desc) (bs |> fromBytes) with
+    | Success(arg, is) -> 
+        arg |> should equal res
+        is.Position.Offset |> should equal bs.Length
+    | _ -> failwithf "Test failed: %A %A %A" desc bs res
--- Lib/Disassembler.fs
+++ Lib/Disassembler.fs
@@ -59,6 +59,79 @@ module Disassembler =
           (MregT7, 7) ]
         |> Map.ofList
     
+    let descRegMap = 
+        [ ("eAX", AX)
+          ("eCX", CX)
+          ("eBX", BX)
+          ("eDX", DX)
+          ("eSP", SP)
+          ("eBP", BP)
+          ("eSI", SI)
+          ("eDI", DI)
+          ("AX", AX)
+          ("BX", BX)
+          ("CX", CX)
+          ("DX", DX)
+          ("SP", SP)
+          ("BP", BP)
+          ("SI", SI)
+          ("DI", DI)
+          ("CS", CS)
+          ("DS", DS)
+          ("ES", ES)
+          ("SS", SS)
+          ("AL", AL)
+          ("BL", BL)
+          ("CL", CL)
+          ("DL", DL)
+          ("AH", AH)
+          ("BH", BH)
+          ("CH", CH)
+          ("DH", DH) ]
+        |> Map.ofList
+    
+    let aocRegMap = 
+        [ (('b', MregT0), AL)
+          (('b', MregT1), CL)
+          (('b', MregT2), DL)
+          (('b', MregT3), BL)
+          (('b', MregT4), AH)
+          (('b', MregT5), CH)
+          (('b', MregT6), DH)
+          (('b', MregT7), BH)
+          (('v', MregT0), AX)
+          (('v', MregT1), CX)
+          (('v', MregT2), DX)
+          (('v', MregT3), BX)
+          (('v', MregT4), SP)
+          (('v', MregT5), BP)
+          (('v', MregT6), SI)
+          (('v', MregT7), DI)
+          (('w', MregT0), AX)
+          (('w', MregT1), CX)
+          (('w', MregT2), DX)
+          (('w', MregT3), BX)
+          (('w', MregT4), SP)
+          (('w', MregT5), BP)
+          (('w', MregT6), SI)
+          (('w', MregT7), DI)
+          (('p', MregT0), AX)
+          (('p', MregT1), CX)
+          (('p', MregT2), DX)
+          (('p', MregT3), BX)
+          (('p', MregT4), SP)
+          (('p', MregT5), BP)
+          (('p', MregT6), SI)
+          (('p', MregT7), DI) ]
+        |> Map.ofList
+    
+    let modRegMap = 
+        [ (MregT0, ES)
+          (MregT1, CS)
+          (MregT2, SS)
+          (MregT3, DS) ]
+        |> Map.ofList
+    
     /// puint8 :: Parser<Word8>
     let pword8 = satisfy (fun _ -> true) "any byte"
     
@@ -107,6 +180,69 @@ module Disassembler =
         pword8
         >>= (fun w8 -> f <!> parseReg w8 <*> parseRmArgs w8)
         >>= createpModRegRm
+    
+    /// pargument :: string -> Parser<Argument>
+    let pargument (desc : string) : Parser<Argument> = 
+        let getRegister aoc r = aocRegMap.[(aoc, r)] |> ArgRegister
+        
+        let parseDrefOrReg aoc = 
+            pmodRegRm |>> (fun mrm -> 
+            match mrm.ModRM with
+            | RmaReg r -> getRegister aoc r
+            | RmaDeref d -> ArgDereference d)
+        
+        let parseImmediate8 = W8
+                              >> ArgImmediate
+                              <!> pword8
+        let parseImmediate16 = W16
+                               >> ArgImmediate
+                               <!> pword16
+        
+        let parseNonRegArgs = 
+            function 
+            | [ 'E'; aoc ] -> parseDrefOrReg aoc
+            | [ 'G'; aoc ] -> pmodRegRm |>> (fun mrm -> mrm.ModReg |> getRegister aoc)
+            | [ 'J'; 'b' ] -> W8
+                              >> ArgOffset
+                              <!> pword8
+            | [ 'J'; 'v' ] -> W16
+                              >> ArgOffset
+                              <!> pword16
+            | [ 'I'; 'b' ] -> parseImmediate8
+            | [ 'I'; 'v' ] -> parseImmediate16
+            | [ 'I'; 'w' ] -> parseImmediate16
+            | [ 'I'; '0' ] -> parseImmediate8
+            | [ 'O'; _ ] -> 
+                pword16 |>> (fun w16 -> 
+                ArgDereference { DrefType = MrmTDisp
+                                 DrefDisp = 
+                                     w16
+                                     |> W16
+                                     |> Some })
+            | [ 'S'; 'w' ] -> pmodRegRm |>> (fun mrm -> modRegMap.[mrm.ModReg] |> ArgRegister)
+            | [ 'A'; 'p' ] -> 
+                pword16 .>>. pword16 |>> (fun (o, s) -> 
+                { Offset = o
+                  Segment = s }
+                |> ArgAddress)
+            | [ '1' ] -> 
+                1uy
+                |> ArgConstant
+                |> returnP
+            | [ '3' ] -> 
+                3uy
+                |> ArgConstant
+                |> returnP
+            | [ 'M'; 'p' ] -> parseDrefOrReg 'p'
+            | _ -> failwithf "DESC value = %A is unexpected." desc
+        match descRegMap |> Map.tryFind desc with
+        | Some r -> 
+            r
+            |> ArgRegister
+            |> returnP
+        | None -> 
+            desc
+            |> Seq.toList
+            |> parseNonRegArgs
 /// popCode :: InstructionSet -> Parser<string * string[]>
-/// pargument :: Parser<Argument>
 /// pinstruction :: Parser<Instruction>
